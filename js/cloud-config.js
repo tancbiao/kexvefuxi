14cc02cf8818f1579a487a09b1ebb6ea8f5e1e86
tancbiao <1572403714@qq.com>
2026-05-19 19:41:59 +0800

    fix: 更新隧道 URL（新隧道已可用）

--- js/cloud-config.js
+++ js/cloud-config.js
@@ -9,7 +9,7 @@
  * 如遇连接问题，检查腾讯云安全组是否开放 443 端口
  */
 
-const API_BASE = 'https://thanks-families-sweet-anytime.trycloudflare.com/api';
+const API_BASE = 'https://hourly-highest-lips-beneath.trycloudflare.com/api';
 
 function setCloudKey(key) {}
 function getCloudKey() { return ''; }
